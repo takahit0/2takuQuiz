3ff5b9b8468ee3e9fd7f4e480b1fe33b38c83847
Oya Takahito <taka1114@me.com>
2021-08-31 21:13:28 +0900

    Initial Commit

---
+++ 2択クイズアプリ/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2択クイズアプリ
+//
+//  Created by 大矢貴仁 on 2021/08/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
